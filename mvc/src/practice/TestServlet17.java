68833147eb79bae33cb14f5a1cd69e87f48ea342
neutral <neutral@NN0434>
2022-09-01 10:07:54 +0900

    17かえた

--- mvc/src/practice/TestServlet17.java
+++ mvc/src/practice/TestServlet17.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+//かえた
 @WebServlet("/TestServlet17")
 public class TestServlet17 extends HttpServlet {
     private static final long serialVersionUID = 1L;
